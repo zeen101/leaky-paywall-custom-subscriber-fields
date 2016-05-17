021b5e657a79ad4952ca824bdb45ac473ed46091
Mike Selander <mikeselander@gmail.com>
2016-05-17 12:21:39 +0200

    Patch applied - on every instance of get_user_meta we now check for noth prefixed and unprefixed values

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * @since 1.0.0
  */
 
-if ( !function_exists( 'wp_print_r' ) ) { 
+if ( !function_exists( 'wp_print_r' ) ) {
 
 	/**
 	 * Helper function used for printing out debug information
@@ -16,13 +16,29 @@ if ( !function_exists( 'wp_print_r' ) ) {
 	 * @param int $args Arguments to pass to print_r
 	 * @param bool $die TRUE to die else FALSE (default TRUE)
 	 */
-    function wp_print_r( $args, $die = true ) { 
-	
+    function wp_print_r( $args, $die = true ) {
+
         $echo = '<pre>' . print_r( $args, true ) . '</pre>';
-		
+
         if ( $die ) die( $echo );
         	else echo $echo;
-		
-    }   
-	
+
+    }
+
+}
+
+function get_leaky_user_meta( $user_id, $key ){
+	global $which_leaky_paywall;
+
+	// Try for the new meta string first
+	$meta = get_user_meta( $user_id, $which_leaky_paywall . $key, true );
+
+	// If that returned nothing, try for an un-prefixed meta string
+	if ( empty( $meta ) ){
+		$meta = get_user_meta( $user_id, $key, true );
+	}
+
+	// Return whichever result returned, if any
+	return $meta;
+
 }
